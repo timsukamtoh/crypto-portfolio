98425f88167f807028f6d09542c970030b205919
Tim Sukamtoh <timtoh168@gmail.com>
2024-02-27 10:13:41 -0700

    updated links

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -2,33 +2,23 @@ export const profile = {
   name: "Tim",
   role: "Software Engineer",
   bio: "I'm a software engineer that loves building products and teaching others how to do the same.",
-  imageUrl: "https://i.imgur.com/2m4XIqN.png",
+  imageUrl: "https://i.imgur.com/aXxC4Uv.png",
   address: "0x8d176E070D1EFA11CF0f00B2d271bC12a3Ef783e",
   links: [
     {
       name: "Twitter",
-      url: "https://twitter.com/seroxdesigns",
+      url: "https://twitter.com/timmys_eth_",
       icon: "twitter",
     },
     {
       name: "GitHub",
-      url: "https://github.com/Seroxdesign",
+      url: "https://github.com/timsukamtoh",
       icon: "",
     },
     {
       name: "LinkedIn",
-      url: "https://www.linkedin.com/in/sero-346b85202/",
+      url: "https://www.linkedin.com/in/timothy-sukamtoh/",
       icon: "",
-    },
-    {
-      name: "Blog",
-      url: "https://blog.serotonindesigns.com",
-      icon: "",
-    },
-    {
-      name: 'Podcast',
-      url: 'https://open.spotify.com/show/2HfqI2dFN85uLLFwGqEk0d?si=f918a9d9ea6f4d38',
-      icon: ''
     }
   ],
   projects: [

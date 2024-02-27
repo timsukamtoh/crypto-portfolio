a15798a25a39a84d6560d775bd18754ea569a09b
Tim Sukamtoh <timtoh168@gmail.com>
2024-02-27 10:22:31 -0700

    update wallet addreess

--- src/components/SendTransaction/index.tsx
+++ src/components/SendTransaction/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { useSendTransaction } from 'wagmi' 
-import { parseEther } from 'viem' 
- 
-export function SendTransaction() {
-  const { data: hash, sendTransaction, isPending} = useSendTransaction() 
+import { useSendTransaction } from 'wagmi'
+import { parseEther } from 'viem'
 
-  async function submit(e: React.FormEvent<HTMLFormElement>) { 
-    e.preventDefault() 
+export function SendTransaction() {
+  const { data: hash, sendTransaction, isPending} = useSendTransaction()
+
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
     const formData = new FormData(e.target as HTMLFormElement)
-    const value = formData.get('value') as string 
-    sendTransaction({ to: '0xc0163E58648b247c143023CFB26C2BAA42C9d9A9', value: parseEther(value) }) 
-  } 
+    const value = formData.get('value') as string
+    sendTransaction({ to: '0x8d176E070D1EFA11CF0f00B2d271bC12a3Ef783e', value: parseEther(value) })
+  }
 
   return (
     <form onSubmit={submit} className="flex flex-col items-center justify-center space-y-4">
